f63034704cc0884f5c04310dfcf7f566df5ee639
murtaza98 <m.patrawala@somaiya.edu>
2020-07-11 20:50:44 +0530

    fix leave manager approve bug

--- src/frontend/EMSFrontend/src/app/leave_components/leave-manager/leave-manager.component.ts
+++ src/frontend/EMSFrontend/src/app/leave_components/leave-manager/leave-manager.component.ts
@@ -1,83 +1,79 @@
-import { Component, Input, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { Component, Input, OnInit, QueryList, ViewChildren, OnDestroy } from '@angular/core';
+import { LeaveApplication } from '@modules/auth/models/leaveApplication.model';
 import { SBSortableHeaderDirective, SortEvent } from '@modules/tables/directives';
-import { Subscription, Observable } from 'rxjs';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Observable, Subscription } from 'rxjs';
 
 import { LeaveappService } from '../services/leaveapp.service';
-import { LeaveApplication } from '@modules/auth/models/leaveApplication.model';
-import { NgbModal,ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'sb-leave-manager',
-  templateUrl: './leave-manager.component.html',
-  styleUrls: ['./leave-manager.component.scss']
+    selector: 'sb-leave-manager',
+    templateUrl: './leave-manager.component.html',
+    styleUrls: ['./leave-manager.component.scss'],
 })
-export class LeaveManagerComponent implements OnInit {
-  @Input() pageSize = 4;
+export class LeaveManagerComponent implements OnInit, OnDestroy {
+    constructor(public leaveappService: LeaveappService, private modalService: NgbModal) {}
+    @Input() pageSize = 4;
 
-  leaveapps$!: Observable<LeaveApplication[]>;
-  total$!: Observable<number>;
-  sortedColumn!: string;
-  sortedDirection!: string;
-  closeResult!:string;
-  la!:string;
-  subscription: Subscription = new Subscription();
+    leaveapps$!: Observable<LeaveApplication[]>;
+    total$!: Observable<number>;
+    sortedColumn!: string;
+    sortedDirection!: string;
+    closeResult!: string;
+    la!: string;
+    subscription: Subscription = new Subscription();
 
-  @ViewChildren(SBSortableHeaderDirective) headers!: QueryList<SBSortableHeaderDirective>;
+    @ViewChildren(SBSortableHeaderDirective) headers!: QueryList<SBSortableHeaderDirective>;
+    leave!: LeaveApplication;
 
-  constructor(public leaveappService: LeaveappService,
-                    private modalService:NgbModal) {}
-
-  ngOnInit() {
-      this.leaveappService.pageSize = this.pageSize;
-      this.leaveapps$ = this.leaveappService.leaveapps$;
-      this.total$ = this.leaveappService.total$;
-      this.subscription.add(this.leaveappService.getEmpLeave());
-      // console.log(localStorage.getItem('Auth-User'));
-  }
-
-  onSort({ column, direction }: SortEvent) {
-      this.sortedColumn = column;
-      this.sortedDirection = direction;
-      this.leaveappService.sortColumn = column;
-      this.leaveappService.sortDirection = direction;
-  }
-  leave!:LeaveApplication;
-  open(content:any,leave:LeaveApplication) {
-    this.leave = leave;
-    console.log(leave.id);
-    this.la = leave.id;
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-}
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
+    ngOnInit() {
+        this.leaveappService.pageSize = this.pageSize;
+        this.leaveapps$ = this.leaveappService.leaveapps$;
+        this.total$ = this.leaveappService.total$;
+        this.subscription.add(this.leaveappService.getEmpLeave());
+        // console.log(localStorage.getItem('Auth-User'));
     }
-  }
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
 
-  approve(){
-    console.log("here "+this.la);
-    // let leave = this.leaveappService.getLeaveById(this.la);
-    console.log("in component "+this.leave.approved);
-    if(this.leave.approved === 'false'){
-      console.log("inside if");
-      this.leave.approved = 'true';
-    }else{
-      this.leave.approved = 'false';
+    onSort({ column, direction }: SortEvent) {
+        this.sortedColumn = column;
+        this.sortedDirection = direction;
+        this.leaveappService.sortColumn = column;
+        this.leaveappService.sortDirection = direction;
     }
-   
-    console.log("in component "+this.leave.approved);
-    this.leaveappService.saveLeave(this.leave);
-  }
+    open(content: any, leave: LeaveApplication) {
+        this.leave = leave;
+        console.log(leave.id);
+        this.la = leave.id;
+        this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+            result => {
+                this.closeResult = `Closed with: ${result}`;
+            },
+            reason => {
+                this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+            }
+        );
+    }
+
+    private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        } else {
+            return `with: ${reason}`;
+        }
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
+
+    approve() {
+        if ((this.leave.approved as any) === false) {
+            this.leave.approved = 'true';
+        } else {
+            this.leave.approved = 'false';
+        }
+        this.leaveappService.saveLeave(this.leave);
+    }
 }
--- src/frontend/EMSFrontend/src/app/leave_components/services/leaveappdb.service.ts
+++ src/frontend/EMSFrontend/src/app/leave_components/services/leaveappdb.service.ts
@@ -1,58 +1,51 @@
-import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
 import { LeaveApplication } from '@modules/auth/models/leaveApplication.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class LeaveappdbService {
-  httpOptions = {
-    headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': 'http://localhost:8080',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-    }),
-};
+    httpOptions = {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': 'http://localhost:8080',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+        }),
+    };
 
-  url = 'http://localhost:8080/leaveapplications';
-   constructor(private http: HttpClient) {
-    this.http = http;
-   }
-   
-  public getAll(): Observable<LeaveApplication[]> {
-    return this.http.get<LeaveApplication[]>(this.url, this.httpOptions);
-  }
-  public getById(id:string): Observable<LeaveApplication[]> {
-    return this.http.get<LeaveApplication[]>(this.url+'/'+id, this.httpOptions);
-  }
-  public createLeave(leaveapp:LeaveApplication){
-    return this.http
-    .post(this.url,leaveapp,this.httpOptions)
-    .subscribe(la=>{
-      console.log('createLeave returned '+ la)
-    });
-  }
-  public saveLeave(leaveapp:LeaveApplication){
-    return this.http
-    .put(this.url,leaveapp,this.httpOptions)
-    .subscribe(la=>{
-      console.log('saveLeave returned '+ la)
-    });
-  }
-  public empLeave(id:string): Observable<LeaveApplication[]> {
-    return this.http.get<LeaveApplication[]>(this.url+'/'+id+'/leave', this.httpOptions);
-  }
-  public getLeaveById(id:string): Observable<LeaveApplication> {
-    return this.http.get<LeaveApplication>(this.url+'byid/'+id,
-     this.httpOptions);
-  }
-  public delete(id:string){
-    console.log("in leavedb service "+this.url+'/'+id);
-    this.http.delete(this.url+'/'+id,this.httpOptions)
-    .subscribe(data=>{
-      // console.log("data returned by delete is "+data);
-  });;
-  }
-  
+    url = 'http://localhost:8080/leaveapplications';
+    constructor(private http: HttpClient) {
+        this.http = http;
+    }
+
+    public getAll(): Observable<LeaveApplication[]> {
+        return this.http.get<LeaveApplication[]>(this.url, this.httpOptions);
+    }
+    public getById(id: string): Observable<LeaveApplication[]> {
+        return this.http.get<LeaveApplication[]>(this.url + '/' + id, this.httpOptions);
+    }
+    public createLeave(leaveapp: LeaveApplication) {
+        return this.http.post(this.url, leaveapp, this.httpOptions).subscribe(la => {
+            console.log('createLeave returned ' + la);
+        });
+    }
+    public saveLeave(leaveapp: LeaveApplication) {
+        return this.http.put(this.url, leaveapp, this.httpOptions).subscribe(la => {
+            console.log('saveLeave returned ' + la);
+        });
+    }
+    public empLeave(id: string): Observable<LeaveApplication[]> {
+        return this.http.get<LeaveApplication[]>(this.url + '/' + id + '/leave', this.httpOptions);
+    }
+    public getLeaveById(id: string): Observable<LeaveApplication> {
+        return this.http.get<LeaveApplication>(this.url + 'byid/' + id, this.httpOptions);
+    }
+    public delete(id: string) {
+        console.log('in leavedb service ' + this.url + '/' + id);
+        this.http.delete(this.url + '/' + id, this.httpOptions).subscribe(data => {
+            // console.log("data returned by delete is "+data);
+        });
+    }
 }
